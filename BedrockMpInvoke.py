649eb3df992faee684da03431b1ae81457e189d8
Yeshwanth101 <54802370+Yeshwanth101@users.noreply.github.com>
2025-06-20 17:23:36 +0000

    Update BedrockMpInvoke.py

--- BedrockMpInvoke.py
+++ BedrockMpInvoke.py
@@ -2,20 +2,32 @@ import boto3
 import json
 
 # Initialize the Bedrock runtime client
-bedrock = boto3.client('bedrock-runtime', region_name='us-west-2') # Replace with your region
+bedrock = boto3.client('bedrock-runtime', region_name='us-east-1') # Replace with your region
 
 # Define the model ID for Meta Llama 3.1 8B Instruct
-model_id = 'meta.llama3-1-8b-instruct-v1:0'
+model_id = 'arn:aws:sagemaker:us-east-1:196856463470:endpoint/endpoint-quick-start-nwm3o'
 
 # Prepare the input prompt
 prompt = "Explain the significance of the water cycle in Earth's ecosystem."
 
 # Construct the request body
 body = {
-"prompt": prompt,
+"messages": [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant that answers in JSON ormat with a key 'message'."
+    },
+    {
+        "role": "user",
+        "content": "Tell me a fun fact about giraffes."
+    },
+],
 "max_gen_len": 512,
 "temperature": 0.7,
 "top_p": 0.9
+}
+
+print(body)p_p": 0.9
 }
 
 # Invoke the model
